dc8130160f789f0a3d419087a788451544d15131
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-27 04:40:30 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -177,7 +177,7 @@ export default function Home() {
       const RecordRTC = (window as any).RecordRTC;
       const StereoAudioRecorder = (window as any).StereoAudioRecorder;
       // Access the microphone and start recording
-      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      if (navigator.mediaDevices) {
         navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
 
           const reconnectWebSocket = () => {
