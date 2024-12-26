c87fae40f2a15c3c75c3457b0df553313a902397
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-26 14:20:02 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -149,7 +149,7 @@ export default function Home() {
     }
   }
 
-  const SOCKET_URL = "wss://audio.enty.services/stream";
+  const SOCKET_URL = "wss://gtp.aleopool.cc/stream";
 
   // Initialize WebSocket and media devices
   useEffect(() => {
